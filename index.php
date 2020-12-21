ece65f467f4713c45ed8d7bc3e0a7c5d6c9957d9
Mehran <mehran.git@hotmail.com>
2020-12-21 13:08:12 -0500

    add flexbox to index.php

--- index.php
+++ index.php
@@ -6,42 +6,26 @@
 
                 <div class = "post">
 
-                    <img class="img-principal" src = "Images/accueil.png"
-                         alt = "Le U Sports a ajouté une année supplémentaire d’admissibilité afin de permettre aux étudiants de poursuivre leur carrière d’athlètes l’an prochain.">
-                    <br><br>
-                    <h1>Informations - Des repas santé pour vos enfants</h1>
-                    <p>
-                        <strong>Lunch écolier Mehran</strong> est un service alimentaire spécialisé dans la cuisine pour enfants,
-                        en milieux primaire et préscolaire (CPE, garderies et écoles).
-                    </p>
-
-                    <p>
-                        Riche de 20 ans d'expérience dans l'industrie alimentaire, notre succès se base sur des repas
-                        sains, équilibrés et aimés des enfants. Ceux-ci demeurent toujours préparés le jour même, sans
-                        décongélation, et conservent toute leur saveur et leur consistance. De plus, nos ingrédients de
-                        haute qualité proviennent de distributeurs ou producteurs maraîchers locaux, selon la
-                        disponibilité.
-                    </p>
-                    <p>
-                        <strong>Lunch écolier Mehran</strong> offre un service de repas chauds pour écoliers. Avec plus de 20 ans d’expérience, nous avons acquis une expertise enviable dans le domaine. Nous offrons des repas équilibrés et savoureux, tout en excédant les normes spécifiques à votre commissions scolaire et celles du guide alimentaire canadien.
-                    </p>
-                    <p>
-                        Nous offrons différents choix de repas à chaque jour, préparés quotidiennement, sans décongélation. Divers breuvages sont également disponibles. Nos menus sont vérifiés et approuvés par une nutritionniste et les ingrédients utilisés pour leur préparation sont de première qualité.
-                    </p>
-
-                    <h2>Contact</h2>
-                    <div class="contact-container">
-                        <div class = "contact-1">
-                            <img src = "Images/map.png" alt = "">
+                    <div class = "container-a">
+                        <div class="bo-head">
+                            <p>salam mehran</p>
                         </div>
-                        <div class = "contact-2">
-                            <h2>Nos coordonnées :</h2>
-                            <h3>Lunch Écolier Mehran</h3>
-                            <p>11228, rue St Andre St, Montréal, QC, H2R 2G5</p>
-                            <h3>Téléphone :</h3>
-                            <p>(514) 123-4567</p>
-                            <h3>Courriel :</h3>
-                            <p>info@lunch-ecolier-mehran.ca</p>
+
+                    </div>
+                    <div class = "container-b">
+                        <div class = "bo-1">
+                            <p>salam Kamran</p>
+                        </div>
+                        <div class = "bo-2">
+                            <p>salam Kian</p>
+                        </div>
+                    </div>
+                    <div class = "container-c">
+                        <div class = "bo-1">
+                            <p>salam Nikan</p>
+                        </div>
+                        <div class = "bo-2">
+                            <p>salam Ehsan</p>
                         </div>
                     </div>
 
